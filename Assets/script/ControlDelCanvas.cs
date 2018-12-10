568957ed198ad15a6b13554f5903079181cc2608
miguelherrerolopez <miguelherrerolopez2017@gmail.com>
2018-12-10 17:58:06 +0100

    Initial commit

---
+++ Assets/script/ControlDelCanvas.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public interface IMisEventosCanvas : IEventSystemHandler
+{
+    // functions that can be called via the messaging system
+    void MostrarPuntos(int valor);
+}
+
+public class ControlDelCanvas : MonoBehaviour, IMisEventosCanvas
+{
+    public Text texto;
+
+    public void MostrarPuntos(int valor)
+    {
+        texto.text = "Puntos: " + valor;
+    }
+}
---
+++ Assets/script/car.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class car : MonoBehaviour {
+    float velocidad = 0.1f;
+    public KeyCode teclaAsignada = KeyCode.F1;
+    public string nombre;
+    int cuenta = 0;
+    public Text marcador;
+    public Text ganador;
+    public GameObject canvas;
+    bool aterminado = false;
+
+    void Update() {
+        if (Input.GetKeyDown(teclaAsignada)&& aterminado == false) {
+            this.transform.Translate(new Vector3(velocidad, 0, 0));
+            cuenta++;
+            marcador.text = "Tus pulsaciones: " + cuenta;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider) {
+        Debug.Log("El ganador es " + nombre);
+        velocidad = 0;
+        ganador.text = "!Felicidades  " + nombre + " has ganado¡";
+        aterminado = true;
+    }
+}
+
